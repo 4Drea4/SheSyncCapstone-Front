bac97f23d980c2788933048ee5a2389d9592f978
4drea4 <bakerand@outlook.com>
2026-01-31 15:35:16 -0600

    I dont really remember react lol this a little painful having to do this login

--- shesyncfrontend/src/pages/Login/Login.tsx
+++ shesyncfrontend/src/pages/Login/Login.tsx
@@ -3,7 +3,13 @@ import {useNavigate, Link} from 'react-router-dom';
 import './Login.css';
 import type { LoginForm, LoginRes } from '../../types';
 import { login } from '../../api/users';
-import { AuthContext } from '../../context/AuthContext';
+import { AuthProvider } from '../../context/AuthContext';
+
+export default function Login () {
+    const navigate = useNavigate();
+    const { login } = AuthProvider();
+
+}
 
     const [form,setForm] = useState<LoginForm>({
         email: "",
