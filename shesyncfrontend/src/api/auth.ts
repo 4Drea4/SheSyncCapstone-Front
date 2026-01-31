4c23fe3b2d73982f703aa416622ee91fce82acfc
4drea4 <bakerand@outlook.com>
2026-01-30 20:02:55 -0600

    putting together auth

--- shesyncfrontend/src/api/auth.ts
+++ shesyncfrontend/src/api/auth.ts
@@ -1,2 +1,15 @@
 import api from './api';
 
+type Login = {
+    token: string;
+    user:{
+        _id:string;
+        email:string;
+        username:string;
+    };
+};
+
+export async function user{
+    email: string;
+    password:string
+};
