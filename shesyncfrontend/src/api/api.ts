dd118f96e806ff4dadd1d63931185ec954adac13
4drea4 <bakerand@outlook.com>
2026-01-30 19:57:53 -0600

    created an axios instance, created the base url request starting point

--- shesyncfrontend/src/api/api.ts
+++ shesyncfrontend/src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-//referenced log Rocket
+//referenced log Rocket: making network requests to my api
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers:{
