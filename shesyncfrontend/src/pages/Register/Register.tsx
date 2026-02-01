668ddafba5ccf629e5eccd51a19b6f5de95d498f
4drea4 <bakerand@outlook.com>
2026-02-01 16:53:34 -0600

    set state on form and put error in a try catch block, going to jsx after i debug something i just realized was broken

--- shesyncfrontend/src/pages/Register/Register.tsx
+++ shesyncfrontend/src/pages/Register/Register.tsx
@@ -14,4 +14,26 @@ export default function Register(){
     });
 
     const [error, setError] = useState("");
-}
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target;
+        setForm((prev) => ({...prev, [name]: value}));
+    };
+    const handleSubmit = async (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setError("");
+
+        if(!form.username || !form.email || !form.password) {
+            setError("Uh oh you have to fill out each input field love!");
+            return;
+        }
+        try {
+            await userRegister(form.username, form.email, form.password);
+            navigate("/login");
+        } catch (err:any) {
+            setError(err?.response?.data?.message || "Sorry looks like your registration failed, try again");
+        }
+};
+
+    return (
+        
+    )
