69e93ddf30e4bed6801bc04760e3237c9b2e8de9
4drea4 <bakerand@outlook.com>
2026-01-31 18:32:46 -0600

    okay so i realized some of my issues is this double folder thing, and had some debugging, now im styling and i realized this color choice may not be the greatest

--- shesyncfrontend/src/pages/Login/Login.tsx
+++ shesyncfrontend/src/pages/Login/Login.tsx
@@ -4,7 +4,7 @@ import './Login.css';
 import type { LoginForm } from '../../types';
 import { AuthContext } from '../../context/AuthContext';
 import { userLogin } from '../../api/users';
-import sheSyncLogo from '../designassets/logo.png';
+import sheSyncLogo from '/public/logo.png';
 
 export default function Login () {
     const navigate = useNavigate();
