5eaa884ee9a3f61d1e90f71e7ec21a6f9a53bed5
4drea4 <bakerand@outlook.com>
2026-02-02 12:23:20 -0600

    trying to figure out project modal

--- shesyncfrontend/src/components/ProjectModal/ProjectModal.tsx
+++ shesyncfrontend/src/components/ProjectModal/ProjectModal.tsx
@@ -5,5 +5,43 @@ import {createProject } from '../../api/projects';
 
 type ProjectModalProps = {
     onClose: () => void;
+    onCreated: (project: Project) => void; 
+    
+    // dashboard should be able to update and select
+};
 
+export default function ProjectModal({onClose, onCreated} :ProjectModalProps) {
+    const [form, setForm] = useState<CreateProjectInput>({
+        name: "",
+        description: "",
+    });
+
+    const [error, setError] = useState("");
+    const [saving, setSaving] = useState(false);
+
+    function handleChange(
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) {
+        const {name, value} = event.target;
+        setForm((prev) => ({...prev, [name] : value}));
+    }
+    
+    async function handleSubmit( event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        setError("");
+
+        if(!form.name.trim()) {
+            setError("You need a project name");
+            return;
+        }
+
+        try{
+            setSaving(true);
+            const newProject = await createProject({
+                name: form.name.trim(),
+                description: form.description.trim(),
+            });
+
+        }
+    }
 }
