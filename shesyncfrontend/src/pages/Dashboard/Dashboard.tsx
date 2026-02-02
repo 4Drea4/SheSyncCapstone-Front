90ad859f14cce12cb2529c226a150a11691fd38d
4drea4 <bakerand@outlook.com>
2026-02-02 15:15:16 -0600

    debugging

--- shesyncfrontend/src/pages/Dashboard/Dashboard.tsx
+++ shesyncfrontend/src/pages/Dashboard/Dashboard.tsx
@@ -4,26 +4,28 @@ import type { Project } from '../../types';
 import ProjectSelect from '../../components/ProjectSelect/ProjectSelect';
 import './Dashboard.css';
 import sheSyncLogo from '/logo.png'
-import flower from '/public/flowerpink.png'
-
+import flower from '/flowerpink.png'
+import ProjectModal from '../../components/ProjectModal/ProjectModal';
 
 export default function Dashboard() {
     const [projects, setProjects] = useState<Project[]>([]);
     const [selectedProjectId, setSelectedProjectId] = useState("");
     const  [showProjectModal, setShowProjectModal] = useState(false);
-useEffect(()=> {
+
+    useEffect(()=> {
     async function load(){
         const data = await getProjects();
         setProjects(data);
-
     }
     load();
 },[]);
+
         function handleCreated(project:Project) {
             setProjects((prev) => [project, ...prev]);
             // auto select the new project in this list
             setSelectedProjectId(project._id);
         }
+
     return (
         <div className='dashboardPage'>
 
@@ -34,8 +36,8 @@ useEffect(()=> {
                     <span>Master Planning Sountrack →</span>
 
                 {/* add my toggle here */}
-
                  </div>
+
                     <button className='logoutButton' type="button">Logout</button>
                
             </header>
@@ -44,12 +46,13 @@ useEffect(()=> {
             <main className='dashboardMain'>
                 <div className='dashboardHero'>
                     <h1 className="dashboardTitle">Hey girl!</h1> 
+
                     <span className="pinkFlower">
                             <img src={flower} alt ="Flower"/>
                         </span> 
 
                     <p className="dashboardMessage">
-                    Want to get in
+                    Want to get in {" "}
                     <span className="logo">
                             <img src={sheSyncLogo} alt ="She Sync Logo"/>
                         </span> ? <br/> 
@@ -59,30 +62,31 @@ useEffect(()=> {
                        type="button"
                        className="inlineProjectButton"
                        onClick={()
-                        => { setShowProjectModal(true)}>
+                        =>  setShowProjectModal(true)}>
                             New Project +
                         </button> { " "}
                        
                       then you can assign <em>tasks!</em>
                     </p>
 
+                    <div className='dashboardSelectProject'>
+                        <ProjectSelect
+                        projects={projects}
+                        selectedProjectId={selectedProjectId}
+                        onSelect={setSelectedProjectId}
+                        addNew={() => setShowProjectModal(true)}
+                        />
+                    </div>
                 </div>
             </main>
 
-            <div className='dashboardTop'>
-                <h1 className='dashboardTitle'>Project Management Bestie</h1>
-                <ProjectSelect
-            projects={projects}
-            selectedProjectId={selectedProjectId}
-            onSelect={setSelectedProjectId}
-            addNew={()=> setShowProjectModal(true)}
-            />
-            </div>
-           
             {showProjectModal && (
                 // i didnt create the project modal yet
-                <button onClick={()=> setShowProjectModal(false)}>Close This</button>
-            )}
+                <ProjectModal
+                onClose={() => setShowProjectModal(false)}
+                onCreated={handleCreated}
+                />
+                )}    
         </div>
-    )
+    );
 }
