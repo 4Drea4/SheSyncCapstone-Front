1f3d3395f154e49192ca930b76b9e5d91883e61f
4drea4 <bakerand@outlook.com>
2026-02-03 09:30:45 -0600

    tasks owned by projects

--- shesyncfrontend/src/api/tasks.ts
+++ shesyncfrontend/src/api/tasks.ts
@@ -1,2 +1,18 @@
 import api from './api';
 import type {Task, CreateTaskInput} from '../types';
+
+//get my tasks
+export async function getTasks(projectId: string): Promise<Task[]>
+{
+    const res = await api.get<Task[]>(`tasks/project/${projectId}`);
+    return res.data;
+}
+
+//the task in the project
+export async function createTask(
+    projectId: string,
+    input: CreateTaskInput) : Promise<Task> {
+        const res = await api.post<Task>(`/tasks/project/${projectId}`, input);
+
+        return res.data;
+    }
